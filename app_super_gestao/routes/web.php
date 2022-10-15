d678da9a871974ee1b97468ab436ea4690158f16
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-10-15 17:06:35 -0300

    Brincando com as rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Olá, mundo!";
+});
+
+Route::get('/sobre-nos', function () {
+    return "Sobre Nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
 });
