03a6e7dbb51ea799c60050244fdcff79c1926f8e
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-10-15 18:04:53 -0300

    Criação das views

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
-        echo "Contato pelo controller";
+        return  view('site.contato');
     }
 }
---
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,1 @@
+<h3>Sobre Nos (view)</h3>
