d7ee3335d5c6ebb3ebff782a0326c0be3b79b6d6
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-10-17 00:24:01 -0300

    Apelidos para as rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -17,8 +17,20 @@ use Illuminate\Support\Facades\Route;
 //     return "Olá, mundo!";
 // });
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('/login', function(){ return "Login"; })->name('site.login');
+
+Route::prefix('/app')->group( function(){
+
+    Route::get('/clientes', function(){ return "Clientes"; })->name('app.clientes');
+
+    Route::get('/fornecedores', function(){ return "fornecedores"; })->name('site.fornecedores');
+
+    Route::get('/produtos', function(){ return "Produtos"; })->name('site.produtos');
+
+});
