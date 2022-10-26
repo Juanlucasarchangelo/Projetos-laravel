0cda21e3013acfe7ceacb5d31c50e380f2d96534
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-10-26 14:23:35 -0300

    Criando modelos dos menus

--- app_super_gestao/resources/views/site/layouts/basico.blade.php
+++ app_super_gestao/resources/views/site/layouts/basico.blade.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
     </head>
     <body>
+        @include('site.layouts._partials.topo')
         @yield('content');
     </body>
 </html>
