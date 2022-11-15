f2ad8beb908cd20aa87f0735f84e5c475cdf670a
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-11-15 20:21:51 -0300

    Criação das models e migrations

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
+
+        var_dump($_POST);
+
         return  view('site.contato', ['titulo' => 'Contato']);
     }
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
