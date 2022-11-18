aec2e44dde2267ea87996036b2616e692b171506
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-11-18 09:00:23 -0300

    Criação das tabelas filiais e relacionamento entre elas

---
+++ app_super_gestao/database/migrations/2022_11_18_114956_ajuste_produtos_filiais.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AjusteProdutosFiliais extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filiais', function (Blueprint $table) {
+            $table->id();
+            $table->string('filial', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produtos_filiais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id');
+            $table->decimal('preco_venda', 8, 2);
+            $table->integer('preco_minimo');
+            $table->integer('preco_maximo');
+            $table->timestamps();
+
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+
+            Schema::create('produtos', function (Blueprint $table) {
+                $table->dropColumn('preco_venda', 'estoque_minimo', 'estoque_maximo');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
