7315846f4d38e43301e9d941f1e2d02de8a2aac0
juanlucasarchangelo <jlarchangelo@outlook.com>
2022-11-18 08:44:55 -0300

    Criação das tabelas unidade e relacionamento entre elas

---
+++ app_super_gestao/database/migrations/2022_11_18_112949_create_unidades_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
+            $table->timestamps();
+
+            Schema::table('produtos', function(Blueprint $table){
+                $table->unsignedBigInteger('unidade_id');
+                $table->foreign('unidade_id')->references('id')->on('produtos');
+            });
+
+            Schema::table('produtos_detalhes', function(Blueprint $table){
+                $table->unsignedBigInteger('unidade_id');
+                $table->foreign('unidade_id')->references('id')->on('produtos');
+            });
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades');
+    }
+}
