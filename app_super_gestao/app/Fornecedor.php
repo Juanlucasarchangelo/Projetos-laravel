17fe7a0844e43fcbaf08c5562c7080cd8c0c3913
juanlucasarchangelo <jlarchangelo@outlook.com>
2023-08-01 15:51:48 -0300

    Update

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    // Quando o laravel não encontra a tablea, tem que definir ela assim
+    protected $table = 'fornecedores';
+
+    // Permite que o metodo estatico CREATE crie informações nestes campos (Usando o tinker ou metodo statico)
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
