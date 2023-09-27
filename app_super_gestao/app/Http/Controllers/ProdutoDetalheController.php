bd926f22a679fbde8dbd249839b7132215db0ff9
juanlucasarchangelo <jlarchangelo@outlook.com>
2023-09-27 20:40:45 -0300

    Implementação do update e edit

--- app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
@@ -55,24 +55,26 @@ class ProdutoDetalheController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  App\ProdutoDetalhe $produtoDetalhe
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(ProdutoDetalhe $produtoDetalhe)
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto_detalhe.edit', ['produto_detalhe' => $produtoDetalhe, 'unidades' => $unidades]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  App\ProdutoDetalhe $produtoDetalhe
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, ProdutoDetalhe $produtoDetalhe)
     {
-        //
+        $produtoDetalhe->update($request->all());
+        echo 'Atualização feita com sucesso.';
     }
 
     /**
