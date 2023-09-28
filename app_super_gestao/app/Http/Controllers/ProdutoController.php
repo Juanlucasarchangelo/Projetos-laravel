4244637ae925966d1109ef0bc5cd04f296da5358
juanlucasarchangelo <jlarchangelo@outlook.com>
2023-09-27 21:24:56 -0300

    Implementação do relacionamento hasone entre produto e produto detalhes

--- app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Produto;
+use App\ProdutoDetalhe;
 use App\Unidade;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,19 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         $produtos = Produto::paginate(10);
+
+        // foreach($produtos as $key => $produto){
+        //     // print_r($produto->getAttributes());
+
+        //     $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
+
+        //     if(isset($produtoDetalhe)){
+        //         // print_r($produtoDetalhe->getAttributes());
+        //         $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
+        //         $produtos[$key]['largura'] = $produtoDetalhe->largura;
+        //         $produtos[$key]['altura'] = $produtoDetalhe->altura;
+        //     }
+        // }
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all() ]);
     }
--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -26,6 +26,9 @@
                             <th>Descrição</th>
                             <th>Peso</th>
                             <th>Unidade ID</th>
+                            <th>Comprimento</th>
+                            <th>Altura</th>
+                            <th>Largura</th>
                             <th></th>
                             <th></th>
                             <th></th>
@@ -39,6 +42,9 @@
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
+                                <td>{{ $produto->produtoDetalhe->comprimento ?? '' }}</td>
+                                <td>{{ $produto->produtoDetalhe->altura ?? '' }}</td>
+                                <td>{{ $produto->produtoDetalhe->largura ?? '' }}</td>
                                 <td><a href="{{ route('produto.show', ['produto' => $produto->id ]) }}">Visualizar</a></td>
                                 <td>
                                     <form id="form_{{$produto->id}}" method="post" action="{{ route('produto.destroy', ['produto' => $produto->id]) }}">
